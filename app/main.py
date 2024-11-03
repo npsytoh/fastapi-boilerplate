6b6c96753c9cfa689ad12437bebd9064e606ddb7
npsytoh <npsytoh@gmail.com>
2024-11-03 16:32:27 +0900

    feat: Add middleware

--- app/main.py
+++ app/main.py
@@ -1,8 +1,31 @@
+import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI()
+app = FastAPI(
+    debug=False,
+    title="FastAPI App",
+    version="0.1.0",
+)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello FastAPI!"}
+
+
+def main():
+    uvicorn.run(app=app, host="127.0.0.1", port=8000)
+
+
+if __name__ == "__main__":
+    main()
