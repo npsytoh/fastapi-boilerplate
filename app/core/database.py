f90f0aee48ebe5f659c073e456b6c65d35c14cdd
npsytoh <npsytoh@gmail.com>
2024-11-11 23:34:09 +0900

    fix: database.py

--- app/core/database.py
+++ app/core/database.py
@@ -1,9 +1,29 @@
+from collections.abc import Generator
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+try:
+    engine = create_engine(
+        SQLALCHEMY_DATABASE_URL,
+        connect_args={"check_same_thread": False},
+        pool_pre_ping=True,
+        echo=False,
+        future=True,
+    )
+    session_local = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+except Exception:
+    pass
+
 Base = declarative_base()
+
+
+def get_db() -> Generator:
+    try:
+        db = session_local()
+        yield db
+    finally:
+        db.close()
