14ec103dff5f8fbead5fd7b371ebb8bc2961973f
npsytoh <npsytoh@gmail.com>
2024-11-18 00:05:36 +0900

    fix: models/base.py

--- app/models/base.py
+++ app/models/base.py
@@ -1,5 +1,22 @@
-from sqlalchemy.orm import DeclarativeBase
+from datetime import UTC, datetime
+
+from sqlalchemy import Column, DateTime
+from sqlalchemy.ext.declarative import as_declarative, declared_attr
 
 
-class Base(DeclarativeBase):
-    pass
+@as_declarative()
+class Base:
+    @declared_attr
+    def __tablename__(cls):
+        return cls.__name__.lower()
+
+    created_at = Column(DateTime, default=datetime.now(UTC), nullable=False)
+    updated_at = Column(
+        DateTime, default=datetime.now(UTC), onupdate=datetime.now(UTC), nullable=False
+    )
+
+
+class NoTimestampBase(Base):
+    __abstract__ = True
+    created_at = None
+    updated_at = None
--- app/models/user.py
+++ app/models/user.py
@@ -1,6 +1,4 @@
-from datetime import datetime
-
-from sqlalchemy import Column, DateTime, Integer, String
+from sqlalchemy import Column, Integer, String
 
 from app.models.base import Base
 
@@ -11,5 +9,3 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     user_name = Column(String(20), index=True)
     email = Column(String, unique=True, index=True)
-    created_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
-    updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
