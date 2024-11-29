47a10d96302b609662a2f68dfe78bcd6bac6825e
npsytoh <npsytoh@gmail.com>
2024-11-29 23:54:34 +0900

    fix: Update user

--- app/crud/__init__.py
+++ app/crud/__init__.py
@@ -1,1 +1,1 @@
-from .user import create_user, delete_user, get_all_users, get_user_by_id
+from .user import create_user, delete_user, get_all_users, get_user_by_id, update_user
--- app/routers/users.py
+++ app/routers/users.py
@@ -22,6 +22,11 @@ async def create_user(body: schemas.UserRequest, db: AsyncSession = Depends(get_
     return await crud.create_user(db, body)
 
 
+@router.put("/{id}", response_model=schemas.UserResponse)
+async def update_user(id: int, body: schemas.UserRequest, db: AsyncSession = Depends(get_async_db)):
+    return await crud.update_user(db, id, body)
+
+
 @router.delete("/{id}", status_code=204)
 async def delete_user(id: int, db: AsyncSession = Depends(get_async_db)):
     await crud.delete_user(db, id=id)
